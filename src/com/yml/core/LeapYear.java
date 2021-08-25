1202c849362634047729580b38ff430535a198d7
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-08-25 15:01:02 +0530

    leap year Program added

---
+++ src/com/yml/core/LeapYear.java
@@ -0,0 +1,33 @@
+package com.yml.core;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class LeapYear {
+public static void run() {
+		
+		Scanner in = new Scanner(System.in);
+		
+		print("Enter the year");
+		int year = in.nextInt();
+		
+		while( year<1000 || year < 0 || year>=10000) {
+			print("Year should be a 4 digit positive number");
+			year = in.nextInt();
+		}
+		
+		if(((year%4 == 0) && (year%100 != 0)) || (year%400 == 0)) {
+			print(""+year+" is a leap year");
+		}
+		else {
+			print(""+year+" is not a leap year");
+		}
+		
+	}
+	
+	private static void print(String msg) {
+		PrintWriter out = new PrintWriter(System.out);
+		out.println(msg);
+		out.flush();
+	}
+}
